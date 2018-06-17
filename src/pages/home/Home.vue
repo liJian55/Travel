27918dc7a151ac5e93b15bbe8c8493453347ed53
jim <you@example.com>
2018-06-17 11:49:34 +0800

    city-router

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        00
         <home-header :city="city"></home-header>
         <home-swiper :swiperList="swiperList"></home-swiper>
         <HomeIcons :iconList="iconList"></HomeIcons>
