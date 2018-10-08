88d1964b133f9563d85699c1e8e4224bfffb0539
jim <373008676@qq.com>
2018-10-08 22:31:28 +0800

    更新至城市列表页

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,25 @@
 <template>
     <div>
         <CityHeader></CityHeader>
-        
+        <CitySearch></CitySearch>
+        <CityList></CityList>
+        <CityAlphabet></CityAlphabet>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
     name:'City',
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch,
+        CityList,
+        CityAlphabet
+
     }
 }
 </script>
