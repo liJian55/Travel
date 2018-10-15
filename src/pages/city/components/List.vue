0159aba2578ac48aa83361f70d19414c8394bfba
jim <373008676@qq.com>
2018-10-15 21:24:34 +0800

    城市列表左侧

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,73 +12,18 @@
         <div class="area">
             <div class="title border-topbottom">热门城市</div>
             <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
-                </div>
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
-                </div>
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
-                </div>
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
-                </div>
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
-                </div>
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
-                </div>
-                <div class="button-wrapper">
-                    <div class="button bbb">北京</div>     
+                <div class="button-wrapper" v-for="item in hot" :key="item.id">
+                    <div class="button bbb">{{item.name}}</div>     
                 </div>
             </div>
             
         </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
+        <div class="area" v-for="(item, key) in cities" :key="key" :ref="key">
+            <div class="title border-topbottom">{{key}}</div>
             <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom" v-for="innerItem in item" :key="innerItem.id">{{innerItem.name}}</div>
             </div>
-            
         </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-            </div>
-            
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-            </div>
-            
-        </div>
-        <div class="area">
-            <div class="title border-topbottom">A</div>
-            <div class="item-list">
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-                <div class="item border-bottom">阿拉尔</div>
-            </div>    
-          </div>
         </div>
     </div>
 </template>
@@ -87,8 +32,21 @@
 import Bscroll from 'better-scroll'
 export default {
     name:"CityList",
-    mounted(){
+    props: {
+        hot: Array,
+        cities: Object,
+        letter: String
+    },
+    mounted () {
         this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    watch: {
+        letter(){
+            if(this.letter){
+                const element = this.$refs[this.letter][0]
+                this.scroll.scrollToElement(element)
+            }
+        }
     }
 }
 </script>
